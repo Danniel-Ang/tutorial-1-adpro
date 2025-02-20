564c77919378644432542d807b3d12aefc122f1b
Danniel-Ang <danniel@ui.ac.id>
2025-02-20 16:04:16 +0700

    test: one test failed fixed

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -1,13 +1,12 @@
 package id.ac.ui.cs.advprog.eshop;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EshopApplication {
-    private EshopApplication() {}
     public static void main(String[] args) {
         SpringApplication.run(EshopApplication.class, args);
     }
-
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EshopApplicationTests {
-
     @Test
     void contextLoads() {
         EshopApplication.main(new String[]{});
