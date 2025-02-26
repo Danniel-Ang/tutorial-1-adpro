ae5d2cc15634559c062c0b3869daed26c506b1b2
Danniel-Ang <danniel@ui.ac.id>
2025-02-26 15:43:42 +0700

    refactor: i forgot the decorator for repo

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.Iterator;
 
+@Repository
 public class CarRepository {
     static int id = 0;
     private List <Car> carData = new ArrayList<>();
---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.service;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
+
+@Service
+public class CarServiceImpl {
+
+}
