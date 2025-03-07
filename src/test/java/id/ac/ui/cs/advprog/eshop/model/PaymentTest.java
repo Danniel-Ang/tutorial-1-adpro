1b5767f95ff307897b0eea4d50d6338610b09a5d
Danniel-Ang <danniel@ui.ac.id>
2025-03-07 16:49:01 +0700

    [RED] Add tests for Payment model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PaymentTest {
-
     List<Order> orders;
 
     @BeforeEach
@@ -19,11 +18,11 @@ public class PaymentTest {
         List<Product> products;
         products = new ArrayList<>();
         Product product1 = new Product();
-        product1.setProductId("bebekpeking-1c39-460e-8860-71af6af63bd6");
+        product1.setProductId("bebekpeking");
         product1.setProductQuantity(3);
         product1.setProductName("Bebek Peking Goreng");
         Product product2 = new Product();
-        product2.setProductId("ayojoincompetition-4a37-4664-83c7-f32db8620155");
+        product2.setProductId("ayojoincompetition");
         product2.setProductQuantity(2);
         product2.setProductName("Ayam Geprek Join Competition");
         products.add(product1);
@@ -31,9 +30,9 @@ public class PaymentTest {
 
         this.orders = new ArrayList<>();
 
-        Order order1 = new Order("danniel-4a37-4664-83c7-f32db8620155", products, 500L, "Danniel");
-        Order order2 = new Order("bebekpeking-4a37-4664-83c7-f32db8620155", products, 250L, "BebekPeking");
-        Order order3 = new Order("ayojoincompetition-4a37-4664-83c7-f32db8620155", products, 750L, "AyoJoinCompetition");
+        Order order1 = new Order("danniel", products, 500L, "Danniel");
+        Order order2 = new Order("bebekpeking", products, 250L, "BebekPeking");
+        Order order3 = new Order("ayojoincompetition", products, 750L, "AyoJoinCompetition");
         orders.add(order1);
         orders.add(order2);
         orders.add(order3);
@@ -43,10 +42,10 @@ public class PaymentTest {
     void testCreatePaymentSuccessfulVoucher() {
         Map<String, String> paymentDataVoucher = new HashMap<>();
         paymentDataVoucher.put("voucherCode", "ESHOP12345678JOIN");
-        Payment payment1 = new Payment("danniel-aaaa-aaaa-aaaa-aaaaaaaaaaaa", orders.get(1), "VOUCHER", paymentDataVoucher);
+        Payment payment1 = new Payment("danniel", orders.get(1), "VOUCHER", paymentDataVoucher);
         assertSame(this.orders.get(1), payment1.getOrder());
         assertEquals(paymentDataVoucher, payment1.getPaymentData());
-        assertEquals("danniel-aaaa-aaaa-aaaa-aaaaaaaaaaaa", payment1.getId());
+        assertEquals("danniel", payment1.getId());
         assertEquals("VOUCHER", payment1.getMethod());
     }
 
@@ -54,7 +53,7 @@ public class PaymentTest {
     void testCreatePaymentInvalidVoucher() {
         Map<String, String> paymentDataVoucher = new HashMap<>();
         paymentDataVoucher.put("voucherCode", "INVALIDPEKING1234");
-        assertThrows(IllegalArgumentException.class, () -> new Payment("bebekpeking-aaaa-aaaa-aaaa-aaaaaaaaaaaa", orders.get(1), "VOUCHER", paymentDataVoucher));
+        assertThrows(IllegalArgumentException.class, () -> new Payment("bebekpeking", orders.get(1), "VOUCHER", paymentDataVoucher));
     }
 
     @Test
@@ -62,6 +61,6 @@ public class PaymentTest {
         Map<String, String> paymentDataBank = new HashMap<>();
         paymentDataBank.put("bankName", "");
         paymentDataBank.put("referenceCode", "987654");
-        assertThrows(IllegalArgumentException.class, () -> new Payment("ayojoincompetition-aaaa-aaaa-aaaa-aaaaaaaaaaaa", orders.get(1), "BANK_TRANSFER", paymentDataBank));
+        assertThrows(IllegalArgumentException.class, () -> new Payment("ayojoincompetition", orders.get(1), "BANK_TRANSFER", paymentDataBank));
     }
 }
