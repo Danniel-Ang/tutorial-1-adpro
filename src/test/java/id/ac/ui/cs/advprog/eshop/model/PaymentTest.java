fefc8bb8e421ee3723dedea4bf3b0308725e2ff9
Danniel-Ang <danniel@ui.ac.id>
2025-03-07 16:44:56 +0700

    [RED] Add tests for Payment model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -2,58 +2,66 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PaymentTest {
-    private Payment payment;
-    private Map<String, String> paymentData;
+
+    List<Order> orders;
 
     @BeforeEach
     void setUp() {
-        paymentData = new HashMap<>();
-        paymentData.put("bankName", "Bank A");
-        paymentData.put("referenceCode", "123456");
+        List<Product> products;
+        products = new ArrayList<>();
+        Product product1 = new Product();
+        product1.setProductId("bebekpeking-1c39-460e-8860-71af6af63bd6");
+        product1.setProductQuantity(3);
+        product1.setProductName("Bebek Peking Goreng");
+        Product product2 = new Product();
+        product2.setProductId("ayojoincompetition-4a37-4664-83c7-f32db8620155");
+        product2.setProductQuantity(2);
+        product2.setProductName("Ayam Geprek Join Competition");
+        products.add(product1);
+        products.add(product2);
+
+        this.orders = new ArrayList<>();
+
+        Order order1 = new Order("danniel-4a37-4664-83c7-f32db8620155", products, 500L, "Danniel");
+        Order order2 = new Order("bebekpeking-4a37-4664-83c7-f32db8620155", products, 250L, "BebekPeking");
+        Order order3 = new Order("ayojoincompetition-4a37-4664-83c7-f32db8620155", products, 750L, "AyoJoinCompetition");
+        orders.add(order1);
+        orders.add(order2);
+        orders.add(order3);
     }
 
     @Test
-    void testCreatePaymentSuccess() {
-        payment = new Payment("1", "BANK_TRANSFER", "PENDING", paymentData);
-        assertNotNull(payment);
-        assertEquals("BANK_TRANSFER", payment.getMethod());
-        assertEquals("PENDING", payment.getStatus());
+    void testCreatePaymentSuccessfulVoucher() {
+        Map<String, String> paymentDataVoucher = new HashMap<>();
+        paymentDataVoucher.put("voucherCode", "ESHOP12345678JOIN");
+        Payment payment1 = new Payment("danniel-aaaa-aaaa-aaaa-aaaaaaaaaaaa", orders.get(1), "VOUCHER", paymentDataVoucher);
+        assertSame(this.orders.get(1), payment1.getOrder());
+        assertEquals(paymentDataVoucher, payment1.getPaymentData());
+        assertEquals("danniel-aaaa-aaaa-aaaa-aaaaaaaaaaaa", payment1.getId());
+        assertEquals("VOUCHER", payment1.getMethod());
     }
 
     @Test
-    void testCreatePaymentWithEmptyBankName() {
-        paymentData.put("bankName", "");
-        assertThrows(IllegalArgumentException.class, () -> new Payment("1", "BANK_TRANSFER", "PENDING", paymentData));
+    void testCreatePaymentInvalidVoucher() {
+        Map<String, String> paymentDataVoucher = new HashMap<>();
+        paymentDataVoucher.put("voucherCode", "INVALIDPEKING1234");
+        assertThrows(IllegalArgumentException.class, () -> new Payment("bebekpeking-aaaa-aaaa-aaaa-aaaaaaaaaaaa", orders.get(1), "VOUCHER", paymentDataVoucher));
     }
 
     @Test
-    void testCreatePaymentWithEmptyReferenceCode() {
-        paymentData.put("referenceCode", "");
-        assertThrows(IllegalArgumentException.class, () -> new Payment("1", "BANK_TRANSFER", "PENDING", paymentData));
-    }
-
-    @Test
-    void testSetPaymentStatusToSuccess() {
-        payment = new Payment("1", "BANK_TRANSFER", "PENDING", paymentData);
-        payment.setStatus("SUCCESS");
-        assertEquals("SUCCESS", payment.getStatus());
-    }
-
-    @Test
-    void testSetPaymentStatusToRejected() {
-        payment = new Payment("1", "BANK_TRANSFER", "PENDING", paymentData);
-        payment.setStatus("REJECTED");
-        assertEquals("REJECTED", payment.getStatus());
-    }
-
-    @Test
-    void testInvalidPaymentMethod() {
-        assertThrows(IllegalArgumentException.class, () -> new Payment("1", "INVALID_METHOD", "PENDING", paymentData));
+    void testCreatePaymentInvalidBankTransfer() {
+        Map<String, String> paymentDataBank = new HashMap<>();
+        paymentDataBank.put("bankName", "");
+        paymentDataBank.put("referenceCode", "987654");
+        assertThrows(IllegalArgumentException.class, () -> new Payment("ayojoincompetition-aaaa-aaaa-aaaa-aaaaaaaaaaaa", orders.get(1), "BANK_TRANSFER", paymentDataBank));
     }
 }
