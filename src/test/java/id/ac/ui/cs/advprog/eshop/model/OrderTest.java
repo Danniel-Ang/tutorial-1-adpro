b8f44fa93866ff746604d718845bf3917e59b7dc
Danniel-Ang <danniel@ui.ac.id>
2025-03-07 16:47:11 +0700

    [RED] Add tests for Payment model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -1,67 +1,83 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PaymentTest {
+import java.util.ArrayList;
+import java.util.List;
 
-    List<Order> orders;
-    List<Product> products;
-
+public class OrderTest {
+    private List<Product> products;
     @BeforeEach
     void setUp() {
         this.products = new ArrayList<>();
         Product product1 = new Product();
         product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        product1.setProductQuantity(3);
-        product1.setProductName("Bebek Peking Goreng");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(2);
         Product product2 = new Product();
         product2.setProductId("a0f9de46-90b1-437d-a0bf-d0821dde9096");
-        product2.setProductQuantity(2);
-        product2.setProductName("Ayam Geprek Join Competition");
+        product2.setProductName("Sabun Cap Usep");
+        product2.setProductQuantity(1);
         this.products.add(product1);
         this.products.add(product2);
-
-        this.orders = new ArrayList<>();
-
-        Order order1 = new Order("danniel-eb558e9f-1c39-460e-8860-71af6af63bd6", products, 500L, "Danniel");
-        Order order2 = new Order("danniel-a0f9de46-90b1-437d-a0bf-d0821dde9096", products, 250L, "BebekPeking");
-        Order order3 = new Order("a0f9de46-90b1-437d-a0bf-d0821dde9096", products, 750L, "AyoJoinCompetition");
-        orders.add(order1);
-        orders.add(order2);
-        orders.add(order3);
     }
 
     @Test
-    void testCreatePaymentSuccessfulVoucher() {
-        Map<String, String> paymentDataVoucher = new HashMap<>();
-        paymentDataVoucher.put("voucherCode", "ESHOP12345678JOIN");
-        Payment payment1 = new Payment("danniel-a0f9de46-90b1-437d-a0bf-d0821dde9096", orders.get(1), "VOUCHER", paymentDataVoucher);
-        assertSame(this.orders.get(1), payment1.getOrder());
-        assertEquals(paymentDataVoucher, payment1.getPaymentData());
-        assertEquals("danniel-a0f9de46-90b1-437d-a0bf-d0821dde9096", payment1.getId());
-        assertEquals("VOUCHER", payment1.getMethod());
+    void testCreateOrderEmptyProducts() {
+        this.products.clear();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+         Order order = new Order("1234567890-1234567890", this.products, 1000000L,"Danniel");
+        });
     }
 
     @Test
-    void testCreatePaymentInvalidVoucher() {
-        Map<String, String> paymentDataVoucher = new HashMap<>();
-        paymentDataVoucher.put("voucherCode", "INVALIDPEKING1234");
-        assertThrows(IllegalArgumentException.class, () -> new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", orders.get(1), "VOUCHER", paymentDataVoucher));
+    void testCreateOrderDefaultStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, 1708560000L,"Safira Sudrajat");
+
+        assertSame(this.products, order.getProducts());
+        assertEquals(2, order.getProducts().size());
+        assertEquals("Sampo Cap Bambang", order.getProducts().get(0).getProductName());
+        assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductName());
+
+        assertEquals("13652556-012a-4c07-b546-54eb1396d79b", order.getId());
+        assertEquals(1708560000L, order.getOrderTime());
+        assertEquals("Safira Sudrajat", order.getAuthor());
+        assertEquals(OrderStatus.WAITING_PAYMENT.getValue(), order.getStatus());
     }
 
     @Test
-    void testCreatePaymentInvalidBankTransfer() {
-        Map<String, String> paymentDataBank = new HashMap<>();
-        paymentDataBank.put("bankName", "");
-        paymentDataBank.put("referenceCode", "987654");
-        assertThrows(IllegalArgumentException.class, () -> new Payment("a0f9de46-90b1-437d-a0bf-d0821dde9096", orders.get(1), "BANK_TRANSFER", paymentDataBank));
+    void testCreateOrderSuccessStatus() {
+        Order order = new Order(  "13652556-012a-4c07-b546-54eb1396d79b",
+                this.products,  1708560000L,  "Safira Sudrajat", OrderStatus.SUCCESS.getValue());
+
+        assertEquals(OrderStatus.SUCCESS.getValue(), order.getStatus());
+    }
+
+    @Test
+    void testCreateOrderInvalidStatus() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Order order = new Order( "13652556-012a-4c07-b546-54eb1396d79b",
+                    this.products, 1708560000L, "Safira Sudrajat","MEOW");
+        });
+    }
+
+    @Test
+    void testSetStatusToCancelled() {
+        Order order = new Order(  "13652556-012a-4c07-b546-54eb1396d79b",
+                this.products,  1708560000L,  "Safira Sudrajat");
+
+        order.setStatus(OrderStatus.CANCELLED.getValue());
+        assertEquals(OrderStatus.CANCELLED.getValue(), order.getStatus());
+    }
+
+    @Test
+    void testSetStatusToInvalidStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat");
     }
 }
+
